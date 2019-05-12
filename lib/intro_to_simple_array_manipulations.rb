a0b3237bee395cb463e4eef70778bf109911d539
Alexander Schelchere <ascellie@gmail.com>
2019-05-11 22:10:39 -0400

    Skeleton of all the methods

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,44 @@
+def using_push(arr, str)
+
+end
+
+def using_unshift(arr, str)
+
+end
+
+def using_pop(arr)
+
+end
+
+def pop_with_args(arr)
+
+end
+
+def using_shift(arr)
+
+end
+
+def shift_with_args(arr)
+
+end
+
+def using_concat(arr1, arr2)
+
+end
+
+def using_insert(arr, element)
+
+end
+
+def using_uniq(arr)
+
+end
+
+def using_flatten(arr)
+
+end
+
+def using_delete(arr, str)
+
+end
+
