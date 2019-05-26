a6890bfc439927618076e2e4bf0453c57c5260b1
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-05-26 01:00:59 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,36 @@
+def using_push(arr,str)
+  return arr.push(str)
+end
+def using_unshift(arr,str)
+  return arr.unshift(str)
+end 
+def using_pop(arr)
+  return arr.pop 
+end 
+def pop_with_args(arr)
+  return arr.pop(2)
+end 
+def using_shift(arr)
+  return arr.shift 
+end 
+def shift_with_args(arr)
+  return arr.shift(2)
+end 
+def using_concat(arr, arr2)
+  arr.concat(arr2)
+end 
+def using_insert(arr, element)
+  arr.insert(4, element )
+end 
+def using_uniq(arr)
+  return arr.uniq 
+end 
+def using_flatten(arr)
+  return arr.flatten 
+end 
+def using_delete(arr, int )
+  arr.delete(int)
+end 
+def using_delete_at(arr, int)
+  arr.delete_at(int)
+end 
